199b5d2040464dc7dd69635c45354f5356a42675
Udeme01 <emmanueluddy2@gmail.com>
2024-03-05 15:32:12 +0100

    added nav menu  background color change on scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
 import { MobileNav } from "./MobileNav";
 import logo from "../../assets/img/favicon.ico";
 import { DesktopNav } from "./DesktopNav";
-// import { DesktopNav } from "./DesktopNav";
+import { useState } from "react";
+
+const headerStyles = "header xl:w-90 xl:mx-auto xl:p-0";
+const active = "sticky top-0 z-50 bg-white shadow-sm xl:w-90 xl:mx-auto";
 
 export const Header = () => {
+  const [color, setColor] = useState(false);
+  const changeNavColor = () => {
+    let innerHeight = 150;
+    if (window.scrollY >= innerHeight) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavColor);
   return (
-    <header className="header sticky top-0 z-50 bg-white shadow-sm xl:w-90 xl:mx-auto xl:p-0">
+    <header className={color ? active : headerStyles}>
       <div className="flex items-center justify-between">
         <div className="header-left">
           <div className="py-2 px-6">
